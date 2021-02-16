6c6d28a852203f3fb2ad8bdd16f69c961abceb0e
vovka013 <46564695+vovka013@users.noreply.github.com>
2021-02-16 18:19:01 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+
+window.addEventListener('scroll', function() {
+
+
+  	if (pageYOffset>0) {document.getElementById('head').style.opacity = "0.5";}
+  	else{	document.getElementById('head').style.opacity = "1";
+  }
+});
